b70bad647fdb00ec6470660c5ad97d1d94335bc4
Oriel Absin <oriel.absin@gmail.com>
2026-03-01 22:09:53 -0500

    chore: add jsdom mocks for Radix browser APIs
    Mock hasPointerCapture, setPointerCapture, releasePointerCapture,
    scrollIntoView, and ResizeObserver in vitest.setup.ts. These APIs
    are not implemented in jsdom but are required by Radix Select.

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,22 @@
 import '@testing-library/jest-dom/vitest'
+
+// Mock pointer capture APIs (not implemented in jsdom, needed by Radix Select)
+Element.prototype.hasPointerCapture =
+  Element.prototype.hasPointerCapture || (() => false)
+Element.prototype.setPointerCapture =
+  Element.prototype.setPointerCapture || (() => {})
+Element.prototype.releasePointerCapture =
+  Element.prototype.releasePointerCapture || (() => {})
+
+// Mock scrollIntoView (not implemented in jsdom, needed by Radix Select items)
+Element.prototype.scrollIntoView =
+  Element.prototype.scrollIntoView || (() => {})
+
+// Mock ResizeObserver (not implemented in jsdom, needed by Radix popper positioning)
+window.ResizeObserver =
+  window.ResizeObserver ||
+  class {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
